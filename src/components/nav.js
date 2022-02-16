865e4047ff28bfa0c76c7760b9ebcc5859601ea4
Anthony Sikaneta <sikaneta@gmail.com>
2022-02-16 09:11:38 +0200

    Change the refs in the nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,7 +9,7 @@ export default function Nav() {
       <nav className="flex justify-between p-6 px-4">
         <div className="flex justify-between items-center w-full">
           <div className="w-1/2 xl:w-1/3">
-            <Link className="block max-w-max" href="/">
+            <Link className="block max-w-max" to="/">
               <img className="h-8" src={logo} alt="" />
             </Link>
           </div>
@@ -18,7 +18,7 @@ export default function Nav() {
               <li className="mr-12">
                 <Link
                   className="text-coolGray-500 hover:text-coolGray-900 font-medium"
-                  href="/"
+                  to="/"
                 >
                   Skills
                 </Link>
@@ -26,7 +26,7 @@ export default function Nav() {
               <li className="mr-12">
                 <Link
                   className="text-coolGray-500 hover:text-coolGray-900 font-medium"
-                  href="/"
+                  to="/"
                 >
                   Finance
                 </Link>
@@ -34,7 +34,7 @@ export default function Nav() {
               <li className="mr-12">
                 <Link
                   className="text-coolGray-500 hover:text-coolGray-900 font-medium"
-                  href="/"
+                  to="/"
                 >
                   Growth
                 </Link>
@@ -42,7 +42,7 @@ export default function Nav() {
               <li className="mr-12">
                 <Link
                   className="text-coolGray-500 hover:text-coolGray-900 font-medium"
-                  href="/"
+                  to="/"
                 >
                   Jobs
                 </Link>
@@ -50,7 +50,7 @@ export default function Nav() {
               <li className="mr-12">
                 <Link
                   className="text-coolGray-500 hover:text-coolGray-900 font-medium"
-                  href="/"
+                  to="/"
                 >
                   Markets
                 </Link>
